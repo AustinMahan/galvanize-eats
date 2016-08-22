217a8a773f8831482a0063aa4c86c5ef3dfb999d
AustinMahan <amahan0424@gmail.com>
2016-08-22 12:49:17 -0600

    started styling

--- src/js/order.js
+++ src/js/order.js
@@ -9,11 +9,12 @@ $(document).ready(function(){
       types = types.filter(function(item, index, self){
         return self.indexOf(item) == index
       })
+      console.log(types);
       types.forEach(function(type){
         $('select').append(`<optgroup label="${type}" id="${type}"></optgroup>`)
       })
       data.menu.forEach(function(food){
-        $(`#${food.type}`).append(`<option>${food.name} $<span class="price">${food.price}</span></option>`)
+        $(`#${food.type}`).append(`<option>${food.name}- $<span class="price">${food.price}</span></option>`)
       })
       resolve()
     })
@@ -23,11 +24,24 @@ $(document).ready(function(){
     $('.addingItem').click(function(){
       for(var i = 0; i < parseInt($('#quant').val()); i ++){
         console.log('test');
-        $('.cart').append(`<p>${$('select').find(":selected").text()}</p>`);
+        console.log(`<p>${$('select').find(":selected").text()}</p>`);
         var foodPrice = parseFloat($('select').find(":selected").text().substr($('select').find(":selected").text().indexOf('$') + 1))
-        $('.subTot').text(parseFloat($('.subTot').text()) + foodPrice)
-        console.log($('.subTot').text());
+        $('.cart').append(`<p>${$('select').find(":selected").text().substr(0, $('select').find(":selected").text().indexOf('$') - 2)} <span>$${foodPrice}</span></p>`);
+        $('.subTot').text((parseFloat($('.subTot').text()) + foodPrice).toFixed(2))
       }
+      $('.tax').text((parseFloat($('.subTot').text()) * .08).toFixed(2))
+      $('.grand').text((parseFloat($('.subTot').text()) + parseFloat($('.tax').text())).toFixed(2))
+    })
+  })
+
+  $('#deliver').click(function() {
+    $.ajax({
+      url: 'https://galvanize-eats-api.herokuapp.com/orders',
+      method: 'POST'
+    }).done(function(succ){
+      alert(succ)
+    }).fail(function(err){
+      alert(err)
     })
   })
 })
