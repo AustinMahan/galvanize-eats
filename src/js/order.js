09c43830ab01d54e0f398e4e747603b32d208188
AustinMahan <amahan0424@gmail.com>
2016-08-26 15:28:58 -0600

    final commit

--- src/js/order.js
+++ src/js/order.js
@@ -22,27 +22,50 @@ $(document).ready(function() {
 
   theFoodData.then(function() {
     $('.addingItem').click(function() {
-      for(var i = 0; i < parseInt($('#quant').val()); i ++) {
-        console.log('test');
-        console.log(`<p>${$('select').find(":selected").text()}</p>`);
-        var foodPrice = parseFloat($('select').find(":selected").text().substr($('select').find(":selected").text().indexOf('$') + 1));
-        $('.cart').append(`<p>${$('select').find(":selected").text().substr(0, $('select').find(":selected").text().indexOf('$') - 2)} <span>$${foodPrice}</span></p>`);
+      var quant = parseInt($('#quant').val())
+      for(var i = 0; i < quant; i ++) {
+        var selectedOpt = $('select').find(":selected").text()
+        var foodPrice = parseFloat(selectedOpt.substr(selectedOpt.indexOf('$') + 1));
+        $('.cart').append(`<p><i class="fa fa-times"></i>${selectedOpt.substr(0, selectedOpt.indexOf('$') - 2)} <span>$${foodPrice}</span></p>`);
         $('.subTot').text((parseFloat($('.subTot').text()) + foodPrice).toFixed(2));
+        var subTotal = parseFloat($('.subTot').text())
       }
-      $('.tax').text((parseFloat($('.subTot').text()) * .08).toFixed(2));
-      $('.grand').text((parseFloat($('.subTot').text()) + parseFloat($('.tax').text())).toFixed(2));
+      $('.tax').text((subTotal * .08).toFixed(2));
+      $('.grand').text((subTotal + parseFloat($('.tax').text())).toFixed(2));
+
+      $('i').click(function(){
+        $(this).parent().remove()
+        var priceRm = parseFloat($(this).parent().text().substr($(this).parent().text().indexOf('$') + 1))
+        subTotal -= priceRm
+        $('.subTot').text(subTotal.toFixed(2))
+        $('.tax').text((subTotal * .08).toFixed(2));
+        $('.grand').text((subTotal + parseFloat($('.tax').text())).toFixed(2));
+      })
     });
   });
-
   $('#deliver').click(function() {
-    $.ajax({
-      url: 'https://galvanize-eats-api.herokuapp.com/orders',
-      method: 'POST',
-      data: {name: $('#name').val(), address: $('#address').val(), phone: $('#phone').val()}
-    }).done(function(succ) {
-      alert(succ);
-    }).fail(function(err) {
-      alert(err);
-    });
+    var phone = $('#phone').val()
+    phone = phone.split('')
+    phone = phone.filter(function(el){
+      var isNum = parseInt(el)
+      if(isNum == 0 || isNum){
+        return isNum.toString()
+      }
+    })
+    phone = phone.join('')
+    console.log(phone);
+    if(phone.length == 10){
+      $.ajax({
+        url: 'https://galvanize-eats-api.herokuapp.com/orders',
+        method: 'POST',
+        data: {name: $('#name').val(), address: $('#address').val(), phone: $('#phone').val()}
+      }).done(function(succ) {
+        alert(succ);
+      }).fail(function(err) {
+        alert(err);
+      });
+    }else{
+      alert('Enter a valid phone number')
+    }
   });
 });
